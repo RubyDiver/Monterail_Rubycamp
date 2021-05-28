2b53dee43db07d6740289d2ed3f7eab997fed130
Paweł Tatarynowicz <tatarynowiczpawel2@gmail.com>
2021-05-28 11:57:05 +0200

    add faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+CinemaHall.destroy_all
+Seance.destroy_all
+Movie.destroy_all
+
+require 'faker'
+
+(1..15).each do |id|
+  Movie.create!(
+    id: id,
+    name: Faker::Fantasy::Tolkien.poem,
+    genre: Faker::Movie.title
+  )
+end
+
+(1..1).each do |id|
+  CinemaHall.create!(
+      hall_name: Faker::FunnyName.two_word_name,
+      volume: Faker::Number.number(digits: 3 )
+  )
+end
+
+(1..4).each do |id|
+  CinemaHall.create!(
+    hall_name: Faker::FunnyName.two_word_name,
+    volume: Faker::Number.number(digits: 3)
+  )
+end
+
+(1..4).each do |id|
+  CinemaHall.create!(
+    hall_name: Faker::FunnyName.three_word_name,
+    volume: Faker::Number.number(digits: 2)
+  )
+end
+
+(1..1).each do |id|
+  CinemaHall.create!(
+    hall_name: Faker::FunnyName.three_word_name,
+    volume: Faker::Number.number(digits: 2)
+  )
+end
