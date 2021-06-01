e8a3ef7797645e78dc24f07e26d8b71e398dfcd6
Paweł Tatarynowicz <tatarynowiczpawel2@gmail.com>
2021-06-01 14:27:26 +0200

    ...

--- spec/requests/reservations_spec.rb
+++ spec/requests/reservations_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe "`Reservations` requests" do
 
   @movie = Movie.create(name: "sadsd", genre: "horror")
   @cinema_hall = CinemaHall.create(hall_name: "pierwsza", volume: 400)
-  @ticket_desk = TicketDesk.create(name: "Some name", online: true)
   @seance = Seance.create(start_time: "10:00", movie_id: @movie.id, cinema_hall_id: @cinema_hall.id)
+  @ticket_desk = TicketDesk.create(name: "Some name", online: true)
   @reservation = Reservation.create(status: true, seance_id: @seance.id, ticket_desk_id: @ticket_desk.id)
 
   describe "GET /reservations" do
