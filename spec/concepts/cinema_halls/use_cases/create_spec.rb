68a9e582f8d16d54c00f927f2eb1161f560063ee
Paweł Tatarynowicz <tatarynowiczpawel2@gmail.com>
2021-06-11 15:09:21 +0200

    fix cinema hall spec

--- spec/concepts/cinema_halls/use_cases/create_spec.rb
+++ spec/concepts/cinema_halls/use_cases/create_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe CinemaHalls::UseCases::Create do
-  let(:instance) { described_class.new }
+  let(:instance) { described_class.new.call(params: params) }
   let(:params) do
     {
       hall_name: "first",
@@ -9,19 +9,17 @@ RSpec.describe CinemaHalls::UseCases::Create do
     }
   end
 
-  subject { instance.call(params: params) }
-
   describe ".call" do
     it "create cinema hall record" do
-      expect { subject }.to change { CinemaHall.count }.by(1)
+      expect { instance }.to change { CinemaHall.count }.by(1)
     end
 
     it "return cinema hall" do
-      expect(subject).to be_a_kind_of(CinemaHall)
+      expect(instance).to be_a_kind_of(CinemaHall)
     end
 
     it "return Cinema hall with proper values" do
-      expect( subject ).to have_attributes(params)
+      expect(instance).to have_attributes(params)
     end
   end
 end
